f1ca7d20009a734272d684456d820691371ac993
Luciano Ribeiro <luciano.ribeiro@outlook.com>
2022-11-01 15:57:06 -0300

    Setup Frontend

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,6 @@ import { PrismaClient } from "@prisma/client";
 import cors from '@fastify/cors';
 import Fastify from "fastify";
 
-const port = '3333';
-const host = '0.0.0.0'
 
 const prisma = new PrismaClient({
   log: ['query'],
@@ -24,7 +22,7 @@ async function bootstrap() {
     return { count };
   })
 
-  await fastify.listen(port, host);
+  await fastify.listen({port: 3333,})
 }
 
 bootstrap()
